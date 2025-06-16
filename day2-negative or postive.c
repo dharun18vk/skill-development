b528806c20a7fd4e8c29cbdc4ad88a77b1a302b8
dharun18vk <122011887+dharun18vk@users.noreply.github.com>
2025-06-16 22:42:30 +0530

    Create day2-negative or postive.c

---
+++ day2-negative or postive.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    scanf("%d",&num);
+	  if (num > 0){
+            printf("%d\n positive",num);
+        }
+    else if(num == 0){
+      print("zero")}
+	  else{
+	    print("%d negative",num);
+    }
+    return 0;
+}
